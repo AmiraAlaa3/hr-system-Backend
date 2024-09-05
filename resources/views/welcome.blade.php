ecae1df20bfb54ba560e96e559be159d6a903fc9
Amira Ala <ameraalaa641@gmail.com>
2024-09-05 12:49:05 +0300

    create project hr system

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>HR System</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    </head>
+    <body>
+        <h1>WELCOME HR SYSTEM</h1>
+    </body>
+</html>
